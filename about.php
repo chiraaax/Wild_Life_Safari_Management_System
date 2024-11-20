a9885bf1e179bbcceaf874fefc69dddb3751e5e3
DJRaax <chiranjeewalankeshwara@gmail.com>
2024-11-20 07:50:29 +0530

    About.php

--- about.php
+++ about.php
@@ -11,7 +11,7 @@
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
-    <!-- custom css file link -->
+    <!-- Your css file link -->
     <link rel="stylesheet" href="./css/style.css">
 
 </head>
