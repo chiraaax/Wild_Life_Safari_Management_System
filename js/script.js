216bdcfb654239e01a26223da093f45626132521
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2024-12-14 14:49:04 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+
+let userBox = document.querySelector('.header .user-box');
