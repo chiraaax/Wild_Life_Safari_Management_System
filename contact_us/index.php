510debd06f6980595bfd1cb58804d641278063ab
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2024-12-18 15:28:29 +0530

    Update index.php

--- contact_us/index.php
+++ contact_us/index.php
@@ -14,3 +14,25 @@
     
 </head>
 
+<body>
+
+<!-- Start of Header -->
+<section class="header">
+
+    <a href="../home.php" class="logo"><span>S</span>afari <span>L</span>anka</a>
+
+    <nav class="navbar">
+        <a href="../home.php">Home</a>
+        <a href="../about.php">About Us</a>
+        <a href="index.php">Contact Us</a>
+        <a href="../package.php">Our Packages</a>
+        <a href="../reservations/add.php">Reservations</a>   
+        <a href="../payment/add_payment.php">Check Out</a>
+        <a href="../donation/addDonation.php">Donate Now!</a>     
+    </nav>                
+    
+
+</section>
+
+
+<!-- End of Header -->
