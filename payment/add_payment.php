e35822952f4f18815bd408634986c741cf1c0476
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2024-12-31 23:42:11 +0530

    Update add_payment.php

--- payment/add_payment.php
+++ payment/add_payment.php
@@ -1,1 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    include 'db_conn.php'; // Include the database connection file
 
+    // Retrieve form data
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zipcode = $_POST['zipcode'];
+    $cardname = $_POST['cardname'];
+    $cardnumber = $_POST['cardnumber'];
+    $exp_month = $_POST['exp_month'];
+    $exp_year = $_POST['exp_year'];
+
+    // Insert query
+    $sql = "INSERT INTO payments (fullname, email, address, city, state, zipcode, cardname, cardnumber, exp_month, exp_year) 
+            VALUES ('$fullname', '$email', '$address', '$city', '$state', '$zipcode', '$cardname', '$cardnumber', '$exp_month', '$exp_year')";
+
+    if (mysqli_query($conn, $sql)) {
+        // Redirect to success page or perform other actions
+        header("Location: view_payment.php");
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    // Close connection
+    mysqli_close($conn);
+}
+?>
