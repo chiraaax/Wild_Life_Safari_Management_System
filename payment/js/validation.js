7a78d4a2a760f7c5bf131e0370966f90182f666e
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2025-01-02 23:03:52 +0530

    Update validation.js

--- payment/js/validation.js
+++ payment/js/validation.js
@@ -1,1 +1,13 @@
+function validatePaymentForm() {
+    var fullname = document.getElementById("fullname").value;
+    var email = document.getElementById("email").value;
+    var address = document.getElementById("address").value;
+    var city = document.getElementById("city").value;
+    var state = document.getElementById("state").value;
+    var zipcode = document.getElementById("zipcode").value;
+    var cardname = document.getElementById("cardname").value;
+    var cardnumber = document.getElementById("cardnumber").value;
+    var exp_month = document.getElementById("exp_month").value;
+    var exp_year = document.getElementById("exp_year").value;
 
+    var errorMessage = "";
