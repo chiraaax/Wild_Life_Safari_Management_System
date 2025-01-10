088dc5b3c4691b00cdcf343b01d4e9d2c22048af
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2025-01-10 23:32:08 +0530

    Completed about.php

--- Wild_Life_Safari_Management_System/about.php
+++ Wild_Life_Safari_Management_System/about.php
@@ -15,6 +15,9 @@
     <link rel="stylesheet" href="./css/style.css">
 
 </head>
+
+<body>
+
 <body>
 
 <!-- header section starts -->
@@ -36,6 +39,7 @@
 </section>
 
 <!-- header section ends -->
+
 <div class="heading" style="background:url(images/aboutus6.jpg) no-repeat">
     <h1>about us</h1>
 </div>
@@ -69,121 +73,10 @@
 </section>
 <!-- about section ends -->
 
-<!-- reviews section starts -->
-
-<section class="reviews">
-    <div class="swiper reviews-slider">
-        <div class="swiper-wrapper">
-            <div class="swiper-slider slide">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <p>"Safari Lanka exceeded all my expectations! The personalized itinerary allowed us to see so much wildlife, including leopards and elephants. Our guide was incredibly knowledgeable and made the experience truly unforgettable." </p>
-                    <h3>Jake Hunter</h3>
-                    <span>traveler</span>
-                    <img src="images/review1.jpg" alt="traveler">
-                </div>
-            </div>
-            
-
-            <div class="swiper-slider slide">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <p>"I highly recommend Safari Lanka for anyone looking for a well-organized and responsible safari adventure in Sri Lanka. The attention to detail and commitment to conservation were evident throughout our trip." </p>
-                    <h3>Julie hampson</h3>
-                    <span>explorer</span>
-                    <img src="images/review3.jpg" alt="traveler">
-                </div>
-            </div>
-
-                         
-
-            <div class="swiper-slider slide">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <p>"Our family had an amazing time with Safari Lanka. The kids were thrilled to see wild animals in their natural habitat, and we felt safe and well-informed throughout the journey. Can't wait to come back!" </p>
-                    <h3>lorie carl & perk cartson</h3>
-                    <span>travelers</span>
-                    <img src="images/review4.jpg" alt="traveler">
-                </div>
-            </div>
-
-            <div class="swiper-slider slide">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <p>"Booking with Safari Lanka was the best decision. They took care of everything, from arranging accommodations to ensuring we had the best wildlife sightings. It was a seamless and enriching experience." </p>
-                    <h3>Kate jordan</h3>
-                    <span>traveler</span>
-                    <img src="images/review5.jpg" alt="traveler">
-                </div>
-            </div>
-             
-        </div>
-    </div>
-</section>
-<!-- reviews section ends -->
 
 
-<!-- footer section starts -->
-<section class="footer">
 
-    <div class="box-container">
 
-        <div class="box">
-            <h3>Quick links</h3>
-            <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
-            <a href="about.php"><i class="fas fa-angle-right"></i>About Us</a>
-            <a href="package.php"><i class="fas fa-angle-right"></i>Offers & Packages</a>
-            <a href="book.php"><i class="fas fa-angle-right"></i>Reservations</a>
-        </div>
 
-        <div class="box">
-            <h3>Extra links</h3>
-            <a href="./FAQ/FAQ.html"><i class="fas fa-angle-right"></i> Ask Questions</a>
-            <a href="./contact_us/index.php"><i class="fas fa-angle-right"></i> Contact Us </a>
-            <a href="#"><i class="fas fa-angle-right"></i> Privacy Policy</a>
-            <a href="#"><i class="fas fa-angle-right"></i> Terms Of Use</a>
-        </div>
-
-        <div class="box">
-            <h3>Contact info</h3>
-            <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
-            <a href="#"><i class="fas fa-phone"></i> +111-222-3333 </a>
-            <a href="#"><i class="fas fa-envelope"></i> Safarilanka@gmail.com </a>
-            <a href="#"><i class="fas fa-map"></i> Colombo, SriLanka </a>
-        </div>
-
-        <div class="box">
-            <h3>Follow us</h3>
-            <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i> Facebook </a>
-            <a href="https://twitter.com/"><i class="fab fa-twitter"></i> Twitter </a>
-            <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i> Instagram </a>
-            <a href="https://lk.linkedin.com/"><i class="fab fa-linkedin"></i> Linkedin </a>
-        </div>
-
-    </div>
-
-    <div class="credit">Powered By <span><span1>S</span1>afari <span1>L</span1>anka</span> |All rights reserved!</div>
-</section>
-
-<!-- swiper js link -->
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-<!-- custom js file link -->
-<script src="./js/script.js"></script>
 
 </body>
-</html> 
